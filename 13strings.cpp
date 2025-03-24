23a64eede582b6fb66bd9215e52e3ee0a377c3ee
kauntiaakash2 <akashkauntia2006@gmail.com>
2025-03-24 23:21:53 +0530

    strings

---
+++ 13strings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>  // For setprecision
+#include <sstream>  // For stringstream
+#include <string>   // For string
+
+using namespace std;
+
+int main()
+{
+    string item1 = "Hamburger";
+    double price1 = 9.99;
+    string item2 = "Pizza";
+    double price2 = 11.99;
+
+    stringstream menu;
+
+    menu << item1 << " - $" << fixed << setprecision(2) << price1 << endl;
+    menu << item2 << " - $" << fixed << setprecision(2) << price2 << endl;
+
+    cout << menu.str();
+    return 0;
+}
