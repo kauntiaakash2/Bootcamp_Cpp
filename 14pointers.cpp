c08df190796a182fc01456eb1a08a08a8e875c38
kauntiaakash2 <akashkauntia2006@gmail.com>
2025-03-27 22:46:44 +0530

    Pointer_Arithmetic

--- 14pointers.cpp
+++ 14pointers.cpp
@@ -5,6 +5,16 @@ int main(){
     // Pointers are essential in C++ because they allow us to manipulate and access memory directly. With pointers, we can create complex data structures such as linked lists, trees, and graphs, and we can allocate and deallocate memory dynamically. Pointers also provide a way to pass arguments to functions by reference, which can save memory and improve performance.
     int x = 5;
     int *ptr = &x;
-    std::cout<<*ptr<<std::endl;
+    // ptr = x;
+    std::cout<<ptr<<std::endl;
+
+    // arithmetic pointers
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *p = &arr[0];// p points to the first element of the array
+    std::cout<<*p<<std::endl;
+    p++;
+    std::cout<<*p<<std::endl;
+    p += 2; // move the pointer two elements forward
+    std::cout<<*p<<std::endl;
     return 0;
 }
